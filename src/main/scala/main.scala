50c64a553036ec752d0d670124822675d0e8be79
Charles F. Munat <charles.munat@gmail.com>
2011-07-05 17:35:40 -0300

    Round trip enabled.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -17,9 +17,16 @@ import java.util.UUID
 import CCLDSL._
 import PersistedMonadicTS._
 import com.biosimilarity.lift.lib.SpecialKURIDefaults._
+import mTT._
 
 class Main extends RequestRouter {
   val log = new Logger("org.munat.pagent")
+  
+  var rsp: Option[com.biosimilarity.lift.model.store.usage.PersistedMonadicTS.mTT.Resource] = None
+  
+  def setRsp(r: Option[com.biosimilarity.lift.model.store.usage.PersistedMonadicTS.mTT.Resource]) {
+    rsp = r
+  }
   
   implicit def toPattern(
     s : String
@@ -44,32 +51,47 @@ class Main extends RequestRouter {
   }
   
   post("/") = {
+    response.contentType("application/json")
+    
+    val mnkrAgntSrvrLbl = "contentChannel(\"monikerAgentServer\")"
+    
     val clientSessionUuidSnd = "id_" + UUID.randomUUID.toString.replace("-","_")
+    val clntSessSndLbl = "contentChannel(\"" + clientSessionUuidSnd + "\")"
+    
     val clientSessionUuidRec = "id_" + UUID.randomUUID.toString.replace("-","_")
+    val clntSessRcvLbl = "clientSessionRecvChannel(\"" + clientSessionUuidRec + "\")"
+    
     val connectString = "clientSession( sendChan( " + clientSessionUuidSnd + " ), recvChan( " + clientSessionUuidRec + " ) )"
-    val newAgentString = "createNewAgent( ipAddr( " + param.getOrElse("ipAddress", "localhost") + " ), path( " + param.getOrElse("path", "/") + " ) )"
+    
+    val newAgentString = "createNewAgent( ipAddr( " + param.getOrElse("ipAddress", "localhost") + " ), path( " + param.getOrElse("path", """"/"""") + " ) )"
+    
     val queryLogString = "queryLog( session( " + clientSessionUuidSnd + " ), ip( " + param.getOrElse("ipAddress", "localhost") +
-      " ), path(" + param.getOrElse("path", "/") + " ) )"
+      " ), path(" + param.getOrElse("path", """"/"""") + " ) )"
     
     Db.termstore match {
       case Some(ts) => 
         println("+++++ connectString: " + connectString)
-        reset { ts.put(  "monikerAgentServer", connectString ) }
+        reset { ts.put(  mnkrAgntSrvrLbl, connectString ) }
         
         println("+++++ newAgentString: " + newAgentString)
-        reset { ts.put( clientSessionUuidSnd, newAgentString ) }
+        reset { ts.put( clntSessSndLbl, newAgentString ) }
         
         println("WAITING FOR GODOT............................")
-        reset { for( e <- ts.get( clientSessionUuidRec ) ) { handleResponse( e ) } }
+        reset { for( e <- ts.get( clntSessRcvLbl ) ) { setRsp( e ) } }
         println("STILL WAITING............................")
         
         println("+++++ queryLogString: " + queryLogString)
-        reset { ts.put( clientSessionUuidRec, queryLogString ) }
+        reset { ts.put( clntSessRcvLbl, queryLogString ) }
         
       case None => println("+++++ TERMSTORE not found")
     }
     
-    redirect("/")
+    val out = rsp match {
+      case Some(RBound(Some(Ground(r)),_)) => """{"success":true,"message":"""" + r.replace("<![CDATA[","").replace("]]>","") + """"}"""
+      case _ => """{"success":false}"""
+    }
+    
+    out
   }
   
   new ConnectionRouter
